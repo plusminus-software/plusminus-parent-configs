ba8cfc119e9dda65f730d5c56f8db65c5b2b3b27
Taras Shpek <taras.shpek@gmail.com>
2025-06-10 22:31:54 +0300

    update Sonatype repo

---
+++ src/main/java/software/plusminus/configs/PlusminusConfigsPlaceholder.java
@@ -0,0 +1,8 @@
+package software.plusminus.configs;
+
+/**
+ * This interface was added to keep plusminus-config in the Sonatype repository.
+ * Otherwise, the plusminus-configs is removed because of no java classes.
+ */
+public interface PlusminusConfigsPlaceholder {
+}
